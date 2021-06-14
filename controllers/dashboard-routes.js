f480c12b0725d19f08aa84f5de9972944cc29c93
Gurkirat Saini <78436028+GurkiratSaini@users.noreply.github.com>
2021-06-14 07:20:35 -0400

    added dashboard

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,77 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+    if (req.session.loggedIn) {
+        Post.findAll({
+            where: {
+                id: req.session.user_id
+            },
+            include: [
+                {
+                    model: Comment,
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+            .then(dbPostData => {
+                const posts = dbPostData.map(post => post.get({ plain: true }));
+                res.render('dashboard', { posts, loggedIn: req.session.loggedIn });
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            })
+    }
+    else {
+        res.redirect('/login');
+    }
+});
+
+router.get('/edit/:id', (req, res) => {
+    if (req.session.loggedIn) {
+        Post.findOne(
+            {
+                where: {
+                    id: req.params.id
+                },
+                include: [
+                    {
+                        model: Comment,
+                        include: {
+                            model: User,
+                            attributes: ['username']
+                        }
+                    },
+                    {
+                        model: User,
+                        attributes: ['username']
+                    }
+                ]
+            }
+        )
+            .then(dbPostData => {
+                if (dbPostData) {
+                    const post = dbPostData.get({ plain: true });
+
+                    res.render('edit-post', { post, loggedIn: req.session.loggedIn });
+                }
+                else {
+                    res.status(500).json(err);
+                }
+            })
+    }
+    else {
+        res.redirect('/login');
+    }
+});
+
+module.exports = router;
